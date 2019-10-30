6fad7d40998f24471f0e38883c539851ba43a1df
tanglingshu <lingshu118@gmail.com>
2019-10-30 11:02:41 -0700

    edited the repo name

--- bsg_async/bsg_sync_sync/bsg_sync_sync_width_p_64/tcl/oclk_70_FO4.constraints.tcl
+++ bsg_async/bsg_sync_sync/bsg_sync_sync_width_p_64/tcl/oclk_70_FO4.constraints.tcl
@@ -3,5 +3,5 @@ set_clock_uncertainty 0 [get_clock oclk]
 set_input_delay 0 -clock oclk [get_ports iclk_data_i]
 set_load [load_of [get_lib_pin */$::env(LOAD)]] [all_outputs]
 set_output_delay 0 -clock oclk [get_ports oclk_data_o]
-source $::env(PATH)/<new_repo_name>/common/constraints/bsg_async/bsg_async.constraints.tcl
+source $::env(PATH)/bsg_micro_designs/common/constraints/bsg_async/bsg_async.constraints.tcl
 bsg_async
