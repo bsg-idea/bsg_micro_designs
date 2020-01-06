d77268e42cc376436ce20eafb35ea12e5d055965
tanglingshu <lingshu118@gmail.com>
2020-01-06 12:31:24 -0800

    modified file name

--- bsg_noc/bsg_mesh_to_ring_stitch/bsg_mesh_to_ring_stitch_nets_p_4_y_max_p_6_y_max_p_6_width_fwd_p_80_width_back_p_80/tcl/vclk_40_FO4.constraints.tcl
+++ bsg_noc/bsg_mesh_to_ring_stitch/bsg_mesh_to_ring_stitch_nets_p_4_y_max_p_6_y_max_p_6_width_fwd_p_80_width_back_p_80/tcl/vclk_40_FO4.constraints.tcl
@@ -3,5 +3,5 @@ set_clock_uncertainty 0 [get_clock vclk]
 set_input_delay 0 -clock vclk [all_inputs]
 set_load [load_of [get_lib_pin */$::env(LOAD)]] [all_outputs]
 set_output_delay 0 -clock vclk [all_outputs]
-source $::env(PATH)/<new_repo_name>/common/constraints/bsg_async/bsg_async.constraints.tcl
+source $::env(BSG_DIR)/<new_repo_name>/common/constraints/bsg_async/bsg_async.constraints.tcl
 bsg_async
